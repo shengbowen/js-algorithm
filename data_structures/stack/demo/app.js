5c3cf191945dabcdffbf2115bb84015c52af21b6
bruce.sheng <shengbowen@meizu.com>
2017-02-20 10:03:45 +0800

    add stack application

--- data_structures/stack/demo/app.js
+++ data_structures/stack/demo/app.js
@@ -46,3 +46,92 @@ function checkClose(str) {
   }
   return true;
 }
+
+new Vue({
+  el: '#app1',
+  data: {
+    str1: '',
+    error: ''
+  },
+  methods: {
+    handleClick: function() {
+      var _this = this;
+      var str = _this.str1.replace(/s+/,'');
+      if(str === ''){
+        _this.error = '请输入内容！';
+        // console.log(_this.error)
+        return;
+      }
+      _this.error = checkClose(str) ? '' : '没有闭合！';
+    }
+  }
+});
+
+
+/**
+ * 栈的应用2  表达式求值
+   //分配两个栈，ops为运算符栈，nums为数字栈
+  ops = new Stack, nums = new Stack
+  //从表达式中读取字符 直至结束
+  while read c in expression != EOF:
+      //若为左括号，入运算符栈
+      if c is '(':
+          ops.push(c)
+      //若为数字，入数字栈
+      else if c is a number:
+          nums.push(c)
+      //若为操作符
+      else if c is an operator:
+          //若运算符栈的栈顶元素比c的优先级高或一样高，则进行单次运算
+          while ops.top() is equal or precedence over c:
+              op = ops.pop()
+              opn2 = nums.pop()
+              opn1 = nums.pop()
+              //进行单次运算，并把运算数入数字栈
+              nums.push( cal(op,opn1,opn2) )
+      //若为右括号
+      else if c is ')':
+          //除非栈顶元素为左括号，否则运算符栈出栈并将计算结果入数字栈
+          op = ops.pop()
+          while op != '(':
+              opn2 = nums.pop()
+              opn1 = nums.pop()
+              nums.push( cal(op,opn1,opn2) )
+              op = ops.pop()
+  //返回数字栈的栈顶元素
+  return nums.top()
+ */
+
+var calc = (function(){
+  var priority = {
+    '+': 1,
+    '=': 1,
+    '%': 2,
+    '*': 2,
+    '/': 2,
+    '^': 3,
+    '(': 0,
+    ')': 0,
+    '`': -1
+  };
+
+  function doop(op, num1, num2) {
+    switch(op) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        return num1 / num2;
+      case '%':
+        return num1 % num2;
+      case '^':
+        return Math.pow(num1, num2);
+      default:
+        return 0;
+    }
+  }
+
+})();
